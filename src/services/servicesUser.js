79df96516034c83653ad1e64036dc74f488c3717
Daisy <kiennguyen@Kiens-MacBook-Pro.local>
2024-05-14 23:20:35 +0700

    upload

--- src/services/servicesUser.js
+++ src/services/servicesUser.js
@@ -1,32 +1,32 @@
 import axios from "axios";
 
-const getUser = async (query = "") => {
-  try {
-    const response = await axios.get(
-      `https://jsonplaceholder.typicode.com/users${
-        query ? `/${query}/todos` : ""
-      }`
-    );
-    return response.data;
-  } catch (error) {
-    console.log(error);
-    throw error;
-  }
-};
+// const getUser = async (query = "") => {
+//   try {
+//     const response = await axios.get(
+//       `https://jsonplaceholder.typicode.com/users${
+//         query ? `/${query}/todos` : ""
+//       }`
+//     );
+//     return response.data;
+//   } catch (error) {
+//     console.log(error);
+//     throw error;
+//   }
+// };
 
-const patchUser = async (taskId = "") => {
-  try {
-    const response = await axios.patch(
-      `https://jsonplaceholder.typicode.com/todos/${taskId}`,
-      {
-        completed: true,
-      }
-    );
-    return response.data;
-  } catch (error) {
-    console.log(error);
-    throw error;
-  }
-};
+// const patchUser = async (taskId = "") => {
+//   try {
+//     const response = await axios.patch(
+//       `https://jsonplaceholder.typicode.com/todos/${taskId}`,
+//       {
+//         completed: true,
+//       }
+//     );
+//     return response.data;
+//   } catch (error) {
+//     console.log(error);
+//     throw error;
+//   }
+// };
 
 export { getUser, patchUser };
