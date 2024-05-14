2862a5890fe5989d307ea20f6bda2bfa05bf1023
Daisy <kiennguyen@Kiens-MacBook-Pro.local>
2024-05-14 23:16:15 +0700

    update

--- src/services/servicesUser.js
+++ src/services/servicesUser.js
@@ -14,19 +14,19 @@ const getUser = async (query = "") => {
   }
 };
 
-// const patchUser = async (taskId = "") => {
-//   try {
-//     const response = await axios.patch(
-//       `https://jsonplaceholder.typicode.com/todos/${taskId}`,
-//       {
-//         completed: true,
-//       }
-//     );
-//     return response.data;
-//   } catch (error) {
-//     console.log(error);
-//     throw error;
-//   }
-// };
+const patchUser = async (taskId = "") => {
+  try {
+    const response = await axios.patch(
+      `https://jsonplaceholder.typicode.com/todos/${taskId}`,
+      {
+        completed: true,
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
 
 export { getUser, patchUser };
